8698a5c480d972a8fe287693b2a6fbb1fb71892c
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2021-02-11 19:48:06 +0000

    vm-baseline: add option to get gcc flags for each hypv

--- vm-baseline.sh
+++ vm-baseline.sh
@@ -23,6 +23,7 @@ function usage {
 	echo "                          CPU defined in cpu-xml to a guest."
 	echo "    --baseline            Compute a baseline CPU for the given hypervisors."
 	echo "                          This is the default option."
+	echo "    --gcc                 Get gcc optimizations for each hypervisor"
 	exit 1
 }
 
@@ -53,6 +54,28 @@ function cpu_baseline {
 	fi
 }
 
+
+# args: [host...]
+function get_gcc_flags {
+	host=$1
+	if [[ -z "$host" ]]; then
+		echo "gimme a host"
+		exit 1
+	fi
+	# see https://wiki.gentoo.org/wiki/Safe_CFLAGS
+	ssh $host -- gcc -v -E -x c -march=native -mtune=native - < /dev/null 2>&1 \
+		| grep cc1 \
+		| perl -pe 's/ -mno-\S+//g; s/^.* - //g;'
+}
+
+function get_hosts_gcc_flags {
+	for h in $@; do
+		echo " * $h"
+		flags="$(get_gcc_flags $h)"
+		echo "$flags"
+		echo
+	done
+}
 # args: [cpu-file] [host...]
 # copy the baseline config to the remote hosts and compare it with their CPU
 function cpu_compare {
@@ -82,5 +105,10 @@ if test "$1" = "--compare"; then
 	exit 0
 elif test "$1" = "--baseline"; then
 	shift
+elif test "$1" = "--gcc"; then
+	shift
+	get_hosts_gcc_flags "$@"
+	# TODO: compute common flags (I recommend invoking an external python script)
+	exit $?
 fi
 cpu_baseline "$@"
